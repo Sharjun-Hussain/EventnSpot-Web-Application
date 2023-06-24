17aaba7b7838f896eadb44adbd0f7a48bc1e0401
Sharjun Hussain <45225052+Sharjun-Hussain@users.noreply.github.com>
2023-06-24 19:20:14 +0530

    Update AddHall.jsx
    Removed 
    import {set} From mongoose

--- FrontEnd/eventspot/src/Admin/AddHall.jsx
+++ FrontEnd/eventspot/src/Admin/AddHall.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Col, Container, Row, Form, Button } from 'react-bootstrap'
 import ValidateForm from 'form-validation-react';
 import axios from 'axios';
-import { set } from 'mongoose';
+
 import {useNavigate} from 'react-router-dom';
 
 export const AddHall = () => {
